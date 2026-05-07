a43f55147223727b176a79ca516207af2cae5260
A3thyr <a3therys@gmail.com>
2026-05-07 16:53:34 +0300

    init: initialized model Game, its seeding and db:reset script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
+
   return (
     <div className="w-full h-svh flex items-center justify-center">
       <Button>Yo</Button>
+      {games.map((card) => (
+        <Card key={card.id}>
+          <CardTitle>{card.name}</CardTitle>
+        </Card>
+      ))}
     </div>
   );
 }
