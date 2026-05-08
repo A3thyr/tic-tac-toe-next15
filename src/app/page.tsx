4443ce664e2ace1d16bae04a76bb2fc6ff40ae1b
A3thyr <a3therys@gmail.com>
2026-05-08 17:20:26 +0300

    feat: added prettier, tailwind prettier plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { GamesList } from "@/features/games-list/server";
 
 export default async function Home() {
   return (
-    <div className="flex flex-col gap-8 container mx-auto pt-[100px]">
+    <div className="container mx-auto flex flex-col gap-8 pt-[100px]">
       <h1 className="text-4xl font-bold">Игры</h1>
+
       <GamesList />
     </div>
   );
