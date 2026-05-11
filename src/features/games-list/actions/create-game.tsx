92495a82d534e39c4ecfb4806beae70e288b3f28
A3thyr <a3therys@gmail.com>
2026-05-11 18:38:49 +0300

    feat: added redirect on game creation

--- src/features/games-list/actions/create-game.tsx
+++ src/features/games-list/actions/create-game.tsx
@@ -3,6 +3,7 @@
 import { createGame } from "@/entities/game/server";
 import { prisma } from "@/shared/lib/db";
 import { left } from "@/shared/lib/either";
+import { redirect } from "next/navigation";
 
 export const createGameAction = async () => {
   const mockUser = await prisma.user.findFirst();
@@ -10,6 +11,8 @@ export const createGameAction = async () => {
   if (!mockUser) return left("user-not-found" as const);
 
   const gameResult = await createGame(mockUser);
+
+  if (gameResult.type === "right") redirect(`/game/${gameResult.value.id}`);
 
   return gameResult;
 };
